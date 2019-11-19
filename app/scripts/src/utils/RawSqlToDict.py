5a360c10d1e9665f7962b8044d0bd36dd2760b38
Luis Barrera <luis.barrera@schibsted.com>
2019-11-19 10:58:16 -0300

    [database-repo]: Add script that extract feeds

---
+++ app/scripts/src/utils/RawSqlToDict.py
@@ -0,0 +1,37 @@
+import psycopg2
+from .Configure import Conf
+
+def RawSqlToDict(query, param=None):
+    """
+    Funcion para convertir un query raw en un dict
+    Parameters
+    ----------
+    query:
+        Query Raw a utilizar
+    param:
+        En caso de necesitar parametros adicionales pueden ser pasados por aca
+    Returns
+    -------
+    Dict
+        formato [{u'nombre:'valor',N..}]
+    """
+    try:
+        config = Conf( "./utils/resources/configuration.properties" )
+        conn = psycopg2.connect("dbname='"+config.get_specific( 'BLOCKETDB.DATABASE' )+"' user='"+config.get_specific( 'BLOCKETDB.USERNAME' )+"' host='"+config.get_specific( 'BLOCKETDB.HOST' )+"' password='"+config.get_specific( 'BLOCKETDB.PASSWORD' )+"'")
+    except Exception as e:
+        exit(e)
+    cursor = conn.cursor()
+    conn.set_client_encoding('UTF-8')
+    cursor.execute(query, param)
+    fieldnames = [name[0] for name in cursor.description]
+    result = []
+    for row in cursor.fetchall():
+        rowset = []
+        for field in zip(fieldnames, row):
+            rowset.append(field)
+        result.append(dict(rowset))
+    # Closes connection
+    cursor.close()
+    conn.close()
+    # Return results
+    return result
