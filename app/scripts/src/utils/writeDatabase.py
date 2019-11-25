ad2314bf27daa3aee8e5761071a16293aff8430d
Luis Barrera <luis.barrera@schibsted.com>
2019-11-25 11:32:42 -0300

    [database-repo]: Add method that delete data for category

--- app/scripts/src/utils/writeDatabase.py
+++ app/scripts/src/utils/writeDatabase.py
@@ -24,12 +24,45 @@ class writeDatabase(object):
             self.log.getLogError(str(e))
             exit()
 
+
+    def mappingCategory(self, category=None):
+        textCategory = None
+        if (category == 1220): textCategory =  'Comprar'
+        if (category == 1240): textCategory =  'Arrendar'
+        if (category == 2020): textCategory =  'Autos, camionetas y 4x4'
+        if (category == 2060): textCategory =  'Motos'
+        if (category == 3020): textCategory =  'Consolas, videojuegos y accesorios'
+        if (category == 3040): textCategory =  'Computadores y accesorios'
+        if (category == 3060): textCategory =  'Celulares, teléfonos y accesorios'
+        if (category == 3080): textCategory =  'Audio, TV, video y fotografia'
+        if (category == 4020): textCategory =  'Moda y vestuario'
+        if (category == 4040): textCategory =  'Bolsos, bisutería y accesorios'
+        if (category == 4060): textCategory =  'Salud y belleza'
+        if (category == 4080): textCategory =  'Calzado'
+        if (category == 5020): textCategory =  'Muebles'
+        if (category == 5040): textCategory =  'Electrodomésticos'
+        if (category == 5060): textCategory =  'Jardín y herramientas'
+        if (category == 5160): textCategory =  'Otros artículos del hogar'
+        if (category == 6020): textCategory =  'Deportes, gimnasia y accesorios'
+        if (category == 6060): textCategory =  'Bicicletas, ciclismo y accesorios'
+        if (category == 6080): textCategory =  'Instrumentos musicales y accesorios'
+        if (category == 6100): textCategory =  'Música y películas (DVDs, etc.)'
+        if (category == 6120): textCategory =  'Libros y revistas'
+        if (category == 6140): textCategory =  'Animales y sus accesorios'
+        if (category == 6160): textCategory =  'Arte, antigüedades y colecciones'
+        if (category == 6180): textCategory =  'Hobbies y outdoor'
+        if (category == 9020): textCategory =  'Vestuario futura mamá y niños'
+        if (category == 9040): textCategory =  'Juguetes'
+        if (category == 9060): textCategory =  'Coches y artículos infantiles'
+        return textCategory
+
     def deleteCategory(self, schemaTable=None, category=None):
         try:
-            deleteCommand = 'DELETE FROM %s WHERE category = %s ' % (schemaTable, category)
+            textCategory=self.mappingCategory(category)
+            deleteCommand = """DELETE FROM """ + schemaTable + """ WHERE category = '""" + textCategory+ """' """
             self.log.getLogInfo(deleteCommand)
-            cur.execute()
-            cur = self.engine.cursor(deleteCommand)
+            cur = self.engine.cursor()
+            cur.execute(deleteCommand)
             self.engine.commit()
             cur.close()
         except Exception as e:
